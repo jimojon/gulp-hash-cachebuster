236272dcab50c75e6d9af4110984041bfd0f0b4f
Jonas <jonas@positronic.fr>
2016-10-07 13:52:23 +0200

    Remove useless describe in test

--- test/test.js
+++ test/test.js
@@ -4,51 +4,47 @@ var File = require('vinyl');
 var cachebust = require('../');
 
 describe('gulp-hash-cachebuster', function() {
-    describe('in buffer mode', function() {
 
-        it('should emit error on streamed file', function (done) {
-            gulp.src("test/src/index.html", { buffer: false })
-                .pipe(cachebust())
-                .on('error', function (err) {
-                    assert.equal(err.message, 'Streaming not supported');
-                    done();
-                });
-        });
-
-        it('should ignore asset without src', function (done) {
-            var fakeFile = new File({
-                contents: new Buffer('<!DOCTYPE html><html lang="en"><script>console.log("Hello")</script><link></link></html>')
-            });
-
-            var cb = cachebust();
-            cb.write(fakeFile);
-            cb.once('data', function(file)
-            {
-                // make sure it came out the same way it went in
-                assert(file.isBuffer());
-
-                // check the contents
-                assert.equal(file.contents.toString('utf8'), fakeFile.contents);
+    it('should emit error on streamed file', function (done) {
+        gulp.src("test/src/index.html", { buffer: false })
+            .pipe(cachebust())
+            .on('error', function (err) {
+                assert.equal(err.message, 'Streaming not supported');
                 done();
             });
+    });
+
+    it('should ignore asset without src', function (done) {
+        var fakeFile = new File({
+            contents: new Buffer('<!DOCTYPE html><html lang="en"><script>console.log("Hello")</script><link></link></html>')
         });
 
-        it('should ignore non-existent asset', function (done) {
-            var fakeFile = new File({
-                contents: new Buffer('<!DOCTYPE html><html lang="en"><script src="nothing">console.log("Hello")</script><link></link></html>')
-            });
+        var cb = cachebust();
+        cb.write(fakeFile);
+        cb.once('data', function(file)
+        {
+            // make sure it came out the same way it went in
+            assert(file.isBuffer());
 
-            var cb = cachebust();
-            cb.write(fakeFile);
-            cb.once('data', function(file)
-            {
-                // make sure it came out the same way it went in
-                assert(file.isBuffer());
-
-                // check the contents
-                assert.equal(file.contents.toString('utf8'), fakeFile.contents);
-                done();
-            });
+            // check the contents
+            assert.equal(file.contents.toString('utf8'), fakeFile.contents);
+            done();
         });
     });
+
+    it('should emit an error on non-existent asset', function (done) {
+        var fakeFile = new File({
+            contents: new Buffer('<!DOCTYPE html><html lang="en"><script src="nothing">console.log("Hello")</script><link></link></html>')
+        });
+
+        var cb = cachebust();
+        try{
+            cb.write(fakeFile);
+        }catch(e){
+            var message = "Asset not found:";
+            assert.equal(e.message.substr(0, message.length), message);
+            done();
+        }
+    });
+
 });
